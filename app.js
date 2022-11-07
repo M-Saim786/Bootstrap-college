89e7f951d08d5e89494e42d6efb06c33c7785f48
M-Saim786 <saimrashid3344@gmail.com>
2022-11-07 16:09:23 +0500

    Changes

--- app.js
+++ app.js
@@ -9,3 +9,18 @@ else
     toTop.classList.remove('active')
 }
 })
+
+
+const login =()=>
+{
+    var username=document.getElementById('email').value
+    var password=document.getElementById('password').value
+if(username=='admin' && password=='123')
+{
+    alert('Sucess')
+}
+else
+{
+    alert('Login Failed')
+}
+}
