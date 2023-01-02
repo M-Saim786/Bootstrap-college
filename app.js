591d386fa3966c644a309dfea32270600279d3f4
M-Saim786 <saimrashid3344@gmail.com>
2023-01-02 17:36:06 +0500

    2/1/23

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ else
 
 
 var popUp = document.getElementById('popUp')
-console.log(popUp)
+// console.log(popUp)
 setTimeout(() => {
     popUp.setAttribute('style',`display:block;
      transition:all 0.6s`)
@@ -35,3 +35,19 @@ setTimeout(() => {
 // setTimeout(() => {
 //     popUp.setAttribute('style',`display:none`)
 // }, 3000);
+
+var closebtn = document.getElementById('close')
+closebtn.addEventListener('click' ,() =>{
+    popUp.setAttribute("style" , " display:none")
+    // console.log('click')
+})
+
+
+var exampleModal  =document.getElementById('exampleModal')
+
+
+var logClose = document.getElementsByClassName('logClose')
+// console.log(logClose[0])
+logClose[0].addEventListener('click' ,() =>{
+    exampleModal.setAttribute('style' , 'display : none')
+})
