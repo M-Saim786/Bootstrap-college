81ba519e6a69cf06931bb30fb377890b0ec8a074
M-Saim786 <saimrashid3344@gmail.com>
2023-01-05 02:03:18 +0500

    5/1/2023

--- app.js
+++ app.js
@@ -1,53 +1,79 @@
-const toTop = document.querySelector('.top')
-window.addEventListener('scroll' ,() => {
-if(window.pageYOffset>100)
-{
-    toTop.classList.add('active')
-}
-else
-{
-    toTop.classList.remove('active')
-}
-})
+const toTop = document.querySelector(".top");
+window.addEventListener("scroll", () => {
+  if (window.pageYOffset > 100) {
+    toTop.classList.add("active");
+  } else {
+    toTop.classList.remove("active");
+  }
+});
 
-
-const login =()=>
-{
-    var username=document.getElementById('email').value
-    var password=document.getElementById('password').value
-if(username=='admin' && password=='123')
-{
-    alert('Sucess')
-}
-else
-{
-    alert('Login Failed')
-}
-}
-
-
-var popUp = document.getElementById('popUp')
-// console.log(popUp)
+var popUp = document.getElementById("popUp");
 setTimeout(() => {
-    popUp.setAttribute('style',`display:block;
-     transition:all 0.6s`)
+  popUp.setAttribute(
+    "style",
+    `display:block;
+     transition:all 0.6s`
+  );
 }, 1000);
-// setTimeout(() => {
-//     popUp.setAttribute('style',`display:none`)
-// }, 3000);
+setTimeout(() => {
+  popUp.setAttribute("style", `display:none`);
+}, 10000);
 
-var closebtn = document.getElementById('close')
-closebtn.addEventListener('click' ,() =>{
-    popUp.setAttribute("style" , " display:none")
-    // console.log('click')
-})
+// First Popup ka close
+var closebtn = document.getElementById("close");
+closebtn.addEventListener("click", () => {
+  popUp.setAttribute("style", `display:none`);
+});
 
+var Email = document.getElementById("Email");
+var password = document.getElementById("Password");
+var firstName = document.getElementById("fname");
+var lastName = document.getElementById("lname");
 
-var exampleModal  =document.getElementById('exampleModal')
+// Firebase Login Connect Database
+var loginBtn = document.getElementById("loginBtn");
+loginBtn.addEventListener("click", () => {
+  if (Email[0].value == "" || password[0].value == "") {
+    alert("Please Enter User Name or Password ");
+  } else {
+    // firebase
+    //   .auth()
+    //   .signInWithEmailAndPassword(Email[0].value, password[0].value)
+    //   //
+    //   .then(function (user) {
+    //     console.log(user);
+    // var userobj = {
+    //   Email : Email[0].value,
+    //   password: password[0].value,
+    //   FirstName : firstName.value,
+    //   lastName : lastName.value
+    // };
+    // firebase.database().ref("user/").push(userobj);
+    // })
+    // .catch(function (e) {
+    //   alert(e.message);
+    // });
+  }
+});
 
-
-var logClose = document.getElementsByClassName('logClose')
-// console.log(logClose[0])
-logClose[0].addEventListener('click' ,() =>{
-    exampleModal.setAttribute('style' , 'display : none')
-})
+// Firebase SignUP Connect Database
+var SignUP = document.getElementById("SignUP");
+SignUP.addEventListener("click", () => {
+  firebase
+    .auth()
+    .createUserWithEmailAndPassword(Email.value, password.value)
+    .then((user) => {
+      console.log(user);
+      var UserInfoObj = {
+        FirstName: firstName.value,
+        lastName: lastName.value,
+        Email: Email.value,
+        password: password.value,
+      };
+      firebase.database().ref("user/").push(UserInfoObj);
+    })
+    .catch((e) => {
+      console.log(e);
+      alert(e.message);
+    });
+});
